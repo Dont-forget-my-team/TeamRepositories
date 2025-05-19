cfe09217e6f0e344ed585f7799731eb74748dd1a
omj0301 <dhalswo30@gmail.com>
2025-05-19 16:26:22 +0900

    캘린더_수정_버튼과 지출내역 추가_오민재

--- app/src/main/java/com/example/calendar/CalendarDate.java
+++ app/src/main/java/com/example/calendar/CalendarDate.java
@@ -1,15 +1,24 @@
 package com.example.calendar;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CalendarDate {
     public int day;
     public boolean isCurrentMonth;
-    public String expense;
-    public String income;
+    public boolean isSelected;
+    public List<Integer> expenses;
 
-    public CalendarDate(int day, boolean isCurrentMonth, String expense, String income) {
+    public CalendarDate(int day, boolean isCurrentMonth) {
         this.day = day;
         this.isCurrentMonth = isCurrentMonth;
-        this.expense = expense;
-        this.income = income;
+        this.expenses = new ArrayList<>();
+    }
+    public String getTotalExpenseString() {
+        int total = 0;
+        for (int e : expenses) {
+            total += e;
+        }
+        return total == 0 ? "" : "-₩" + total;
     }
 }
